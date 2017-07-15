b6324575d7dbd6405e92346125a97ce190e5fc17
ChrisKlinglerOhsome <christopherklingler@Christophers-MacBook-Pro.local>
2017-07-15 00:38:32 -0600

    Initial commit

---
+++ gen/com/webnmobilesolutions/cameraarray/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.webnmobilesolutions.cameraarray;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
